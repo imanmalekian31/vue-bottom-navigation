d280954c10dfcc351add3b47ae53428078733c7a
iman malekian <imanmalekian31@gmail.com>
2021-09-09 15:10:19 +0430

    fix(icon): :fire: Remove js icon import

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import "@fortawesome/fontawesome-free/css/all.css";
-import "@fortawesome/fontawesome-free/js/all.js";
 
 Vue.config.productionTip = false;
 

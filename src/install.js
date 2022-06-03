b3be3ba52c27c4baad3fe372fbc30f18354839fd
Iman malekian <imanmalekian31@gmail.com>
2022-06-03 17:39:54 +0430

    fix(install): :bug: Fix global installing

--- src/install.js
+++ src/install.js
@@ -4,6 +4,16 @@ import SwipeBottomNavigation from "./components/SwipeBottomNavigation.vue";
 import RingBottomNavigation from "./components/RingBottomNavigation.vue";
 import WindowsBottomNavigation from "./components/WindowsBottomNavigation.vue";
 
+export default {
+  install(Vue) {
+    Vue.component("CurvedBottomNavigation", CurvedBottomNavigation);
+    Vue.component("GrowBottomNavigation", GrowBottomNavigation);
+    Vue.component("SwipeBottomNavigation", SwipeBottomNavigation);
+    Vue.component("RingBottomNavigation", RingBottomNavigation);
+    Vue.component("WindowsBottomNavigation", WindowsBottomNavigation);
+  },
+};
+
 export {
   CurvedBottomNavigation,
   GrowBottomNavigation,

86b2615cbc754cf823accccac251abc43d6c0f1e
iman malekian <imanmalekian31@gmail.com>
2022-11-04 14:31:19 +0330

    refactor(rollup): :recycle: Refactor build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,31 @@
-import vue from "rollup-plugin-vue";
-import css from "rollup-plugin-import-css";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import { terser } from "rollup-plugin-terser";
+import vue from 'rollup-plugin-vue'
+import css from 'rollup-plugin-import-css'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import { terser } from 'rollup-plugin-terser'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default [
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: [
       {
-        format: "esm",
-        file: "dist/library.mjs",
+        format: 'esm',
+        file: 'dist/library.mjs',
+        exports: 'named'
       },
       {
-        format: "cjs",
-        file: "dist/library.js",
-      },
+        format: 'cjs',
+        file: 'dist/library.js',
+        exports: 'named'
+      }
     ],
+    external: ['vue-router'],
     plugins: [
       vue(),
       peerDepsExternal(),
-      css({ minify: true, output: "dist/style.css" }),
+      css({ minify: true, output: 'dist/style.css' }),
       terser(),
-    ],
-  },
-];
+      nodeResolve()
+    ]
+  }
+]

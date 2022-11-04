fe7c695f730a4e8e4e790cb124db058e34224284
iman malekian <imanmalekian31@gmail.com>
2022-11-04 14:32:22 +0330

    feat(eslint): :sparkles: Add eslint config

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import CurvedBottomNavigation from "./components/CurvedBottomNavigation.vue";
-import GrowBottomNavigation from "./components/GrowBottomNavigation.vue";
-import RingBottomNavigation from "./components/RingBottomNavigation.vue";
-import SwipeBottomNavigation from "./components/SwipeBottomNavigation.vue";
-import WindowsBottomNavigation from "./components/WindowsBottomNavigation.vue";
+import CurvedBottomNavigation from './components/CurvedBottomNavigation.vue'
+import GrowBottomNavigation from './components/GrowBottomNavigation.vue'
+import RingBottomNavigation from './components/RingBottomNavigation.vue'
+import SwipeBottomNavigation from './components/SwipeBottomNavigation.vue'
+import WindowsBottomNavigation from './components/WindowsBottomNavigation.vue'
 
 const plugin = {
-  install(Vue) {
-    Vue.component("CurvedBottomNavigation", CurvedBottomNavigation);
-    Vue.component("GrowBottomNavigation", GrowBottomNavigation);
-    Vue.component("SwipeBottomNavigation", SwipeBottomNavigation);
-    Vue.component("RingBottomNavigation", RingBottomNavigation);
-    Vue.component("WindowsBottomNavigation", WindowsBottomNavigation);
-  },
-};
+  install (Vue) {
+    Vue.component('CurvedBottomNavigation', CurvedBottomNavigation)
+    Vue.component('GrowBottomNavigation', GrowBottomNavigation)
+    Vue.component('SwipeBottomNavigation', SwipeBottomNavigation)
+    Vue.component('RingBottomNavigation', RingBottomNavigation)
+    Vue.component('WindowsBottomNavigation', WindowsBottomNavigation)
+  }
+}
 
 export {
   CurvedBottomNavigation,
   GrowBottomNavigation,
   RingBottomNavigation,
   SwipeBottomNavigation,
-  WindowsBottomNavigation,
-};
+  WindowsBottomNavigation
+}
 
-export default plugin;
+export default plugin

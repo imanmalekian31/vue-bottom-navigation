5a027ff31eb26113137b45261e19a86c7f67c80f
iman malekian <imanmalekian31@gmail.com>
2022-12-02 15:31:12 +0330

    feat(prettier): :sparkles: Add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import CurvedBottomNavigation from "./components/CurvedBottomNavigation.vue";
-import GrowBottomNavigation from "./components/GrowBottomNavigation.vue";
-import RingBottomNavigation from "./components/RingBottomNavigation.vue";
-import SwipeBottomNavigation from "./components/SwipeBottomNavigation.vue";
-import WindowsBottomNavigation from "./components/WindowsBottomNavigation.vue";
+import CurvedBottomNavigation from './components/CurvedBottomNavigation.vue';
+import GrowBottomNavigation from './components/GrowBottomNavigation.vue';
+import RingBottomNavigation from './components/RingBottomNavigation.vue';
+import SwipeBottomNavigation from './components/SwipeBottomNavigation.vue';
+import WindowsBottomNavigation from './components/WindowsBottomNavigation.vue';
 
 const plugin = {
   install(Vue: any) {
-    Vue.component("CurvedBottomNavigation", CurvedBottomNavigation);
-    Vue.component("GrowBottomNavigation", GrowBottomNavigation);
-    Vue.component("SwipeBottomNavigation", SwipeBottomNavigation);
-    Vue.component("RingBottomNavigation", RingBottomNavigation);
-    Vue.component("WindowsBottomNavigation", WindowsBottomNavigation);
+    Vue.component('CurvedBottomNavigation', CurvedBottomNavigation);
+    Vue.component('GrowBottomNavigation', GrowBottomNavigation);
+    Vue.component('SwipeBottomNavigation', SwipeBottomNavigation);
+    Vue.component('RingBottomNavigation', RingBottomNavigation);
+    Vue.component('WindowsBottomNavigation', WindowsBottomNavigation);
   },
 };
 

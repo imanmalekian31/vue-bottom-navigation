bb796dc43004887fee14351f2e086d162d02f60b
Iman malekian <imanmalekian31@gmail.com>
2023-05-17 20:10:58 +0330

    feat(install): :art: Add vue type

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
+import { App, Plugin } from 'vue';
+
 import CurvedBottomNavigation from './components/CurvedBottomNavigation.vue';
 import GrowBottomNavigation from './components/GrowBottomNavigation.vue';
 import RingBottomNavigation from './components/RingBottomNavigation.vue';
 import SwipeBottomNavigation from './components/SwipeBottomNavigation.vue';
 import WindowsBottomNavigation from './components/WindowsBottomNavigation.vue';
 import HillBottomNavigation from './components/HillBottomNavigation.vue';
+import FlashlightBottomNavigation from './components/FlashlightBottomNavigation.vue';
 
-const plugin = {
-  install(Vue: any) {
-    Vue.component('CurvedBottomNavigation', CurvedBottomNavigation);
-    Vue.component('GrowBottomNavigation', GrowBottomNavigation);
-    Vue.component('SwipeBottomNavigation', SwipeBottomNavigation);
-    Vue.component('RingBottomNavigation', RingBottomNavigation);
-    Vue.component('WindowsBottomNavigation', WindowsBottomNavigation);
-    Vue.component('HillBottomNavigation', HillBottomNavigation);
+const plugin: Plugin = {
+  install(app: App) {
+    app.component('CurvedBottomNavigation', CurvedBottomNavigation);
+    app.component('GrowBottomNavigation', GrowBottomNavigation);
+    app.component('SwipeBottomNavigation', SwipeBottomNavigation);
+    app.component('RingBottomNavigation', RingBottomNavigation);
+    app.component('WindowsBottomNavigation', WindowsBottomNavigation);
+    app.component('HillBottomNavigation', HillBottomNavigation);
+    app.component('FlashlightBottomNavigation', FlashlightBottomNavigation);
   },
 };
 
@@ -23,6 +27,7 @@ export {
   SwipeBottomNavigation,
   WindowsBottomNavigation,
   HillBottomNavigation,
+  FlashlightBottomNavigation,
 };
 
 export default plugin;
